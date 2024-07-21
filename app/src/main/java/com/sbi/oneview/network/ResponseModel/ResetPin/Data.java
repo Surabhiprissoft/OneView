8a2423de4cd11d80db5071e4b0f36a5e6f54846e
MusadSarguru <musadsarguru@prissoft.net>
2024-07-21 16:15:04 +0530

    added missing file.

---
+++ app/src/main/java/com/sbi/oneview/network/ResponseModel/ResetPin/Data.java
@@ -0,0 +1,37 @@
+package com.sbi.oneview.network.ResponseModel.ResetPin;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Data{
+
+	@SerializedName("message")
+	private String message;
+
+	@SerializedName("targetUrl")
+	private String targetUrl;
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+
+	public void setTargetUrl(String targetUrl){
+		this.targetUrl = targetUrl;
+	}
+
+	public String getTargetUrl(){
+		return targetUrl;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Data{" + 
+			"message = '" + message + '\'' + 
+			",targetUrl = '" + targetUrl + '\'' + 
+			"}";
+		}
+}
